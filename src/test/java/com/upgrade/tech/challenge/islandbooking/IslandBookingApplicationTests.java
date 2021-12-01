5146438d101376cce092cd0148a7b697a572bfb9
fengjiang <jiangfeng1129@gmail.com>
2021-11-30 22:31:00 -0500

    Initial commit

---
+++ src/test/java/com/upgrade/tech/challenge/islandbooking/IslandBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upgrade.tech.challenge.islandbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IslandBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
